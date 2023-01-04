2033bf6c59bf4a356edb799b3bf7b9719c3d367e
Volodymyr Palazanov <76686267+Remus337@users.noreply.github.com>
2023-01-04 14:41:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const connectToDB = require('./db/connect')
+
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+require('dotenv').config()
+app.use(express.static('./public'))
+app.use(express.json());
+
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000
+
+const start = async() => {
+    try {
+        await connectToDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is running at port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
